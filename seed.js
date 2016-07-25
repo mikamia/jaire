3757737343500b868ef44861cc3af197855a29e9
Katherine Buechner <kbuechner@gmail.com>
2016-07-25 15:08:57 -0400

    An inital commit with a basic modified seed

--- seed.js
+++ seed.js
@@ -20,6 +20,7 @@ name in the environment files.
 var chalk = require('chalk');
 var db = require('./server/db');
 var User = db.model('user');
+var Product = db.model('product')
 var Promise = require('sequelize').Promise;
 
 var seedUsers = function () {
@@ -43,9 +44,44 @@ var seedUsers = function () {
 
 };
 
+var seedProducts = function(){
+    var products = [
+    {
+        title: 'Brooklyn Bridge',
+        description: 'A hint of salt with undertones of fresh bike tire and a tang of sweat',
+        price: '100.00',
+        image: 'http://icons.iconarchive.com/icons/icons8/windows-8/512/Astrology-Air-Element-icon.png',
+        tags: '["Brooklyn", "Water", "Urban" ]'
+
+    }, {
+        title: '',
+        description: '',
+        price: '20.00',
+        image: 'http://icons.iconarchive.com/icons/icons8/ios7/256/Astrology-Water-Element-icon.png',
+        tags:'["", "", ""]',
+    }, {
+        title: '',
+        description: '',
+        price: '67.99',
+        image: '',
+        tags: '["", "", ""]'
+    }
+
+    ];
+
+    var creatingProducts = products.map(function(productObj){
+        return Product.create(productObj);
+    });
+
+    return Promise.all(creatingProducts)
+}
+
 db.sync({ force: true })
     .then(function () {
         return seedUsers();
+    })
+    .then(function(){
+        return seedProducts();
     })
     .then(function () {
         console.log(chalk.green('Seed successful!'));
