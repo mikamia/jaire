8bc453f1cd65a8a1f65d58fe3d4a6f719b2ef197
Elisabeth Gross <elisabethgross568@gmail.com>
2016-07-25 16:43:38 -0400

    Product model completed

---
+++ server/db/models/product.js
@@ -0,0 +1,26 @@
+'use strict';
+var crypto = require('crypto');
+var _ = require('lodash');
+var Sequelize = require('sequelize');
+
+var db = require('../_db');
+
+module.exports = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  imageUrl: {
+    type: Sequelize.STRING
+  },
+  tags: {
+    type: Sequelize.ARRAY(Sequelize.STRING)
+  }
+});
