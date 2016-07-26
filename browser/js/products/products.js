76eef39d750c784c0baefd0b9e2f3c7f9e7dc67e
Claire Dranginis <cdranginis@gmail.com>
2016-07-26 18:35:27 -0400

    filtering by tag is partially working

--- browser/js/products/products.js
+++ browser/js/products/products.js
@@ -1,8 +1,35 @@
-app.controller('ProductsController', function($scope, ProductsFactory) {
-  ProductsFactory.getAllProducts()
-  .then(products =>{
-    $scope.products = products;
+app.controller('ProductsController', function($scope, ProductsFactory, $log) {
+  $scope.currCategory = [];
+  $scope.setCategory = function(tag) {
+    $scope.currCategory.push(tag);
+    console.log($scope.currCategory);
+    var promises = $scope.currCategory.map(category=>{
+      return ProductsFactory.getByTag(category);
+    });
+    Promise.all(promises)
+    .then(results =>{
+      results.forEach(result=>{
+        $scope.products = $scope.products.concat(result);
+      });
+    })
+    .catch($log.error);
+  };
+
+  ProductsFactory.getAllTags()
+  .then(tags =>{
+    tags = Array.from(tags);
+    $scope.allTags = tags;
+    console.log(tags);
   });
+
+  if ($scope.currCategory.length === 0) {
+    ProductsFactory.getAllProducts()
+    .then(products =>{
+      $scope.products = products;
+    })
+    .catch($log.error);
+  }
+  
 
 });
 
@@ -14,6 +41,25 @@ app.factory('ProductsFactory', function($http) {
       .then(function(res) {
         return res.data;
       })
+  }
+
+  productsObj.getByTag = function (tag) {
+    return $http.get('/api/products/filter/' + tag)
+    .then(function(res) {
+      return res.data;
+    });
+  }
+
+  productsObj.getAllTags = function(){
+    return this.getAllProducts()
+    .then(products =>{
+      var tags = [];
+      products.forEach(product=>{
+        tags = tags.concat(product.tags);
+      });
+      tags = new Set(tags);
+      return tags;
+    });
   }
 
   return productsObj;
