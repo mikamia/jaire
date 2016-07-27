af8f1f3c19e2b16bdae2bd765484e585973b0b8f
Claire Dranginis <cdranginis@gmail.com>
2016-07-27 18:35:08 -0400

    testing routes is confusing

--- tests/server/routes/products-test.js
+++ tests/server/routes/products-test.js
@@ -4,9 +4,15 @@ var Sequelize = require('sequelize');
 var db = require('../../../server/db');
 var supertest = require('supertest');
 
-xdescribe('Products Route', function () {
+describe('Products Route', function () {
 
-  var app, User, Product, agent;
+  var app, User, Product, agent, ferryProduct;
+
+  ferryProduct = {name: 'Staten Island Ferry',
+        description: 'Smells like fish and desperation.',
+        price: '67.99',
+        imageUrl: '',
+        tags: ["Premium", "Urban", "Water"]};
 
   beforeEach('Sync DB', function () {
     return db.sync({
@@ -26,12 +32,30 @@ xdescribe('Products Route', function () {
       return agent
         .get('/api/products')
         .expect('Content-Type', /json/)
+        .expect(function(res) {
+          console.log(res.body);
+        })
+        .expect(200, [], done);
+    });
+    
+    it('should return products when there are products in the database', function(done) {
+      Product.create(ferryProduct)
+      .then(function(product) {
+        return agent
+        .get('/api/products')
+        .expect(function(res) {
+          console.log(res.body);
+        })
         .expect(200)
-        .expect(function (res) {
-          // res.body is the JSON return object
-          expect(res.body).to.be.an.instanceOf(Array);
-          expect(res.body).to.have.length(0);
-        });
+        .end(done);
+        // .expect function(response) {
+        //   console.log(response.body);
+        //   // delete response[0].createdAt;
+        //   // delete response[0].updatedAt;
+        //   // console.log(response);
+        //   // expect(response).to.eql([ferryProduct]);
+        // }, done);
+      });
     });
   });
 });
