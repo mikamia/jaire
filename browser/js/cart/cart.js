b52ab2c542714d2acc9ab6b95b9dcd427122c99d
Claire Dranginis <cdranginis@gmail.com>
2016-07-28 13:20:58 -0400

    started cart view

---
+++ browser/js/cart/cart.js
@@ -0,0 +1,15 @@
+app.controller('CartController', function($scope, CartFactory, $log) {
+
+});
+
+app.factory('CartFactory', function($http) {
+
+});
+
+app.config(function($stateProvider) {
+  $stateProvider.state('cart', {
+    url: '/cart',
+    templateUrl: 'js/cart/cart.html',
+    controller: 'CartController'
+  });
+});
