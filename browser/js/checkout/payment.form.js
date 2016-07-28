b07601ccfa246f6c01e73c2c3851d6d816787028
Elisabeth Gross <elisabethgross568@gmail.com>
2016-07-28 18:19:00 -0400

    working on hide billing adress

--- browser/js/checkout/payment.form.js
+++ browser/js/checkout/payment.form.js
@@ -3,6 +3,15 @@ app.config(function($stateProvider) {
   $stateProvider.state('checkout', {
     url: '/checkout',
     templateUrl: 'js/checkout/payment.form.html',
-    // controlelr: 'CheckoutCtrl'
+    controlelr: 'CheckoutCtrl'
   });
+});
+
+app.controller('CheckoutCtrl', function($scope) {
+  console.log('hello');
+  $scope.showBillingAddress = function() {
+    console.log($scope.payment.billingAddress);
+    if ($scope.payment.billingAddress) return true;
+    else return false;
+  }
 });
